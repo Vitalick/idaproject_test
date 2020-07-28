cef06e820dcc5dabebad78c318b309cc8970a026
Vitaly Neumoin <vitaly.neumoin@gmail.com>
2020-07-28 18:28:39 +0300

    Закончил

--- image_processing/models.py
+++ image_processing/models.py
@@ -1,16 +1,24 @@
-from django.db import models
+from io import BytesIO
 
-# Create your models here.
+from PIL import Image as PIL_Image
+from django.core.files.base import ContentFile
+from django.db import models
 from django.urls import reverse
 
 
 class Image(models.Model):
-    file = models.ImageField('файл изображения')
-    width = models.PositiveIntegerField('ширина', default=0)
-    height = models.PositiveIntegerField('высота', default=0)
+    file = models.ImageField('файл изображения', upload_to='originals')
+    thumbnail = models.ImageField('превью изображения', blank=True, upload_to='thumbnails')
+    width = models.PositiveIntegerField('ширина', default=100)
+    height = models.PositiveIntegerField('высота', default=100)
 
     def __str__(self):
-        return self.file.name
+        return self.file.name.split('/')[-1]
 
     def get_absolute_url(self):
         return reverse('image_detail', kwargs={'pk': self.id})
+
+    def get_image(self):
+        if self.thumbnail:
+            return self.thumbnail.url
+        return self.file.url
--- image_processing/views.py
+++ image_processing/views.py
@@ -1,10 +1,11 @@
+import os
 from io import BytesIO
 
 import requests
 from PIL import Image as PIL_Image
 from django.core.files.base import ContentFile
 from django.shortcuts import render, redirect
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView
 
 from .models import Image
 
@@ -13,8 +14,38 @@ class ImageList(ListView):
     model = Image
 
 
-class ImageDetail(DetailView):
+class ImageDetail(UpdateView):
     model = Image
+    fields = ['width', 'height']
+    template_name = 'image_processing/image_detail.html'
+
+    def post(self, request, *args, **kwargs):
+        super().post(request, *args, **kwargs)
+        new_width = int(request.POST.get('width', '0'))
+        new_height = int(request.POST.get('height', '0'))
+        thumbnail = PIL_Image.open(self.object.file)
+        format = thumbnail.format
+        if not (new_width is 0 and new_height is 0):
+            ratio = 1
+            if new_width is 0:
+                new_width = thumbnail.width / thumbnail.height * new_height
+            elif new_height is 0:
+                new_height = thumbnail.height / thumbnail.width * new_width
+            else:
+                width_ratio = new_width / thumbnail.width
+                height_ratio = new_height / thumbnail.height
+                if thumbnail.width * width_ratio > new_width or thumbnail.height * width_ratio > new_height:
+                    ratio = height_ratio
+                else:
+                    ratio = width_ratio
+            thumbnail = thumbnail.resize((int(thumbnail.width * ratio), int(thumbnail.height * ratio)), PIL_Image.ANTIALIAS)
+        thumbnail_buffer = BytesIO()
+        thumbnail.save(thumbnail_buffer, format)
+        if os.path.isfile(self.object.thumbnail.path):
+            os.remove(self.object.thumbnail.path)
+        self.object.thumbnail.save(self.object.file.name.split('/')[-1], ContentFile(thumbnail_buffer.getvalue()))
+        thumbnail_buffer.close()
+        return redirect(self.object)
 
 
 class ImageCreate(CreateView):
@@ -44,7 +75,14 @@ class ImageCreate(CreateView):
             img.save(file_buffer, img.format)
             new_obj = Image()
             new_obj.file.save(url.split('/')[-1], ContentFile(file_buffer.getvalue()))
+            new_obj.name = url.split('/')[-1]
             new_obj.save()
             file_buffer.close()
+            thumbnail = PIL_Image.open(new_obj.file)
+            thumbnail.thumbnail((new_obj.width, new_obj.height), PIL_Image.ANTIALIAS)
+            thumbnail_buffer = BytesIO()
+            thumbnail.save(thumbnail_buffer, thumbnail.format)
+            new_obj.thumbnail.save(new_obj.file.name.split('/')[-1], ContentFile(thumbnail_buffer.getvalue()))
+            thumbnail_buffer.close()
             return redirect(new_obj)
         return super().post(request, *args, **kwargs)
